f43e2ded209059557caf308d8d4036ad3f5914ca
danieljwilson <daniel.j.wilson@gmail.com>
2015-05-01 10:41:49 -0400

    Working version ObjectVsTask Uploaded

--- ObjectVsTask/static/js/task.js
+++ ObjectVsTask/static/js/task.js
@@ -41,287 +41,277 @@ $(window).load( function(){
     );
 });
 */
+var lastTrial = 10;
 
-//You can change the upper and lower limit of the win percentage here
-var lowerBound = 0.2;
-var upperBound = 0.8;
-var winPercentageLeft;
-var winPercentageRight;
+var width = 1024;
+var height = 540;
+var left = width*0.7;
+var right = width*0.3;
 
-var randomNumber;
+var rPosition;
+var ePosition;
+var textPosition;
 
-//for stepping through the instructions at the beginning
-var page = 0;
-var introImgBW;
-var introImgColor;
+var howBig = 220;
+var r, g, b;
 
-var nextTrial = false;
-var newRound = true;
-var textNum = 0;
-var textPos;
+var newTrial = true;
+var results = false;
+var even, high, yellow, blue;
 
-//Image arrays
-var images = [];
-var BWimages = [];
-
-//Set the selection keys
-var lettersSmall = [114, 116, 121, 117, 102, 103, 104, 106];
-var lettersBig = [82, 84, 89, 85, 70, 71, 72, 74, 75];
-
-var rightIndex; 
-var leftIndex;
-
-var rightLetterSmall; 
-var rightLetterBig;
-var leftLetterSmall;
-var leftLetterBig;
-
-//For randomizing the left and right of the BW/Color images
-var leftPos = 0+25;
-var rightPos = 512 +12;
-var colorPos; 
-var BWPos;
-
-//For randomizing image order
-var colorImageSelect;
-var BWImageSelect;
-var colorShuffle = [];
-var BWShuffle = [];
-var imageCounter = 1;
-
-//For timing
-var imageOn;
+var rightAnswer;
+var correct;
+var answer;
+var trialOn;
 var elapsedT;
+var selected;
+var numberSelect;
 
-//Recorded data variables
-var trialTotal = 0;
+var trialNumber = 0;
+var win;
 var wins = 0;
 var losses = 0;
-var money = 0;
-var round = 0;
-var trial = 0;
-var win = false;
-var selected;
-var leftSequentialSelect = 0;
-var rightSequentialSelect = 0;
-//also include the bw and color image for the trial
-
-//USING THIS TO EXPLICITLY STATE THE NUMBER OF IMAGES (instead of .length)
-var imageNumber = 20;
-var i;
-
-
-
-function preload() {
-    //LOAD COLOR IMAGES
-  for (i = 0; i< imageNumber; i++ ){
-    images[i] = loadImage( "static/images/color" + i + ".jpg" );   // make sure images "0.jpg" to "11.jpg" exist
-    }
-  //  psiturk.preloadImages(images);
-  //LOAD BW IMAGES
-  for (i = 0; i< imageNumber; i++ ){
-    BWimages[i] = loadImage( "static/images/bw_color" + i + ".jpg" );   // make sure images "0.jpg" to "11.jpg" exist
-    }
-   // psiturk.preloadImages(BWimages);
-}
-
-
-
-/////////////////////////////////////////////////
-//////SETUP/////
-/////////////////////////////////////////////////
 
 function setup() {
+  createCanvas(1024, 540);
 
-  //set screen size
-  createCanvas(1024, 520);
+  left = 0.3*width;
+  right = 0.7*width;
+  sideSelect();
 
-  introImgBW = loadImage("static/images/intro_bw.jpg"); 
-  introImgColor = loadImage("static/images/intro_color.jpg"); 
-
-  
-  round = 0;
-
-  
-  //Initialize Array
-  for (var i = 0; i < imageNumber; i++) {
-    colorShuffle[i] = i;
-    BWShuffle[i] = i;
-  }
-    
-    //underscore has a shuffle method
-    colorShuffle.shuffle();
-    BWShuffle.shuffle();
+  noStroke();
 }
 
 function draw() {
-  //Background color and stroke color
-  background(255);
-  stroke(0);
+  background(102);
+  if (!results) {
 
-  //Each draw cycle sets a different random value between 0 and 1
-  //This is used to decide whether the selection wins or loses
-  //The randomNumber is compared to the selected bandit's winPercentage
-  randomNumber = random(0,1);
+    drawShapes();
+    numberOn();
+    directionsOn();
+    newTrial = false;
 
-  //INSTRUCTIONS: Describe the experiment here
-  if (page==0){
-    instructions();
-
-    if(mouseIsPressed){
-      page+=1;
-    }
-  }
-  
-  //PRACTICE
-  if (page>=1 && page<=3){
-    p5image(introImgColor, width/2+12, 60);
-    p5image(introImgBW, 0+25, 60);
-
-    }
-   
-  if (page==1) {
-    page1();    
-    if (key == 'g' || key == 'G') {
-      page+=1;
-    }
-  }
-  
-  if (page == 2) { 
-      page2();
-      if (key == 'h' || key == 'H') {
-          page+=1;
+    if (key == 'h' || key == 'H') {
+      if (correct == 1) {
+        rightAnswer = true;
+      } else {
+        rightAnswer = false;
       }
-  }
-  
-  if (page ==3) {
-      page3();
+      selected = 1;
+      goToResults();
+    }
+    if (key == 'g' || key == 'G') {
+      if (correct == 2) {
+        rightAnswer = true;
+      } else {
+        rightAnswer = false;
+      }
+      selected = 2;
+      goToResults();
+    }
+    if (key == 'n' || key == 'N') {
+      if (correct == 3) {
+        rightAnswer = true;
+      } else {
+        rightAnswer = false;
+      }
+      selected = 3;
+      goToResults();
+    }
     if (key == 'b' || key == 'B') {
-          page+=1;
-        }   
-  }
-  
-   if (page==4){
-    printRoundTitle();
-
-    if (mouseIsPressed){
-      page+=1;
-      key='a';
-      setLetters();
+      if (correct == 4) {
+        rightAnswer = true;
+      } else {
+        rightAnswer = false;
+      }
+      selected = 4;
+      goToResults();
     }
   }
-  
-  if (page == 5){  
-    //////////////////
-    //THE EXPERIMENT//
-    //////////////////
-    if(newRound){
-      
-        updateRound();
-        newRound = false;
-        imageSelect();
-        //UNCOMMENT...starts timer
-        //imageOn = new Date().getTime();
-        imageOn = millis();
+
+  if (results) {
+    drawShapes();
+    numberOn();
+    directionsOn();
+    if (rightAnswer) {
+      answer = "RIGHT";
+      resultText();
     }
-    
-    if(!newRound){
-       p5image(images[colorImageSelect], colorPos, 60);
-       p5image(BWimages[BWImageSelect], BWPos, 60);
-       
-      if(!nextTrial) {
-        printRound();
-        //FOR LEFT SELECTION
-        if (key == String.fromCharCode(leftLetterSmall) || key == String.fromCharCode(leftLetterBig)) {
-          //TIMING CALCULATION
-          elapsedT = millis() - imageOn;
-          leftSelect();
-         }
-        //FOR RIGHT SELECTION
-        if (key == String.fromCharCode(rightLetterSmall) || key == String.fromCharCode(rightLetterBig)) {
-          //TIMING CALCULATION
-          elapsedT = millis() - imageOn;
-          rightSelect();
-        }
-       
-      upperBanner();
-      text("Select an image.      Left = '" + String.fromCharCode(leftLetterBig) + "', Right ='" + String.fromCharCode(rightLetterBig) + "'", width/2-200, 38);
-      //JAVA VERSION
-      //text("Select an image.      Left = '" + char(leftLetterBig) + "', Right ='" + char(rightLetterBig) + "'", width/2-200, 38);
-     }
-      
-    printEarnings();
-    
-    //TEXT INDICATING WIN/LOSS  
-    //////////////////////////
-     if(nextTrial) {
-       printRound();
-       printOutcome();          
-       upperBanner();
-       text("Press the 'B' key to continue", width/2-135, 38);
-        
-        if(key == 'B' || key == 'b'){
-          nextTrial = false;
-          setLetters();
-
-          //RECORD DATA HERE
-          record_responses(elapsedT, colorPos, BWPos, colorImageSelect, BWImageSelect, trialTotal, round, winPercentageLeft, winPercentageRight, trial, selected, win, wins, losses, money, leftSequentialSelect, rightSequentialSelect);
-          //RESET TIMER
-          imageOn = millis();
-
-          //After 10 trials (or 5 consecutive selections of one bandit) move on to the next round
-          if (trial == 10 || leftSequentialSelect == 5 || rightSequentialSelect == 5){
-            newRound = true;
-            page-=1;
-            leftSequentialSelect =0;
-            rightSequentialSelect = 0;
-          }
-
-          if (trialTotal >= 20){
-            //Then save the data
-            psiTurk.saveData();
-            //Then compute the bonus [this is NOT working yet]
-            psiTurk.computeBonus('compute_bonus');
-            //Then finish which loads the debriefing page
-            finish();
-           }
-            
-        }
-     }
+    if (!rightAnswer) {
+      answer = "WRONG";
+      resultText();
+    }
+    if (keyCode == 32) {
+      results = false;
+      newTrial = true;
+      sideSelect();
     }
   }
+  if (trialNumber >= lastTrial){
+    //Then save the data
+    psiTurk.saveData();
+    //Then compute the bonus [this is NOT working yet]
+    psiTurk.computeBonus('compute_bonus');
+    //Then finish which loads the debriefing page
+    finish();
+    }
 }
+
 
 
 /////////////////////////////////////////////////
 //////// RECORD DATA FUNCTION////////////////////
 /////////////////////////////////////////////////
 
-  function record_responses(elapsedT, colorPos, BWPos, colorImageSelect, BWImageSelect, trialTotal, round, winPercentageLeft, winPercentageRight, trial, selected, win, wins, losses, money, leftSequentialSelect, rightSequentialSelect) {
+  function record_responses(circlePos, squarePos, color, numberSelect, correct, selected, win, elapsedT, wins, losses, trialNumber) {
 
     psiTurk.recordTrialData({ 
-      "Round": round,
-      "Left_Win Percentage": winPercentageLeft,
-      "Right_Wind Percentage": winPercentageRight,
-      "Color Image": colorImageSelect,
-      "BW Image": BWImageSelect,
-      "Color Position": colorPos,
-      "BW Position": BWPos,
-      "Trial": trial,
-      "Selected Image": selected,
-      //UNCOMMENT
+      "Circle Position": circlePos,
+      "Square Position": squarePos,
+      "Number Color": color,
+      "Number Value": numberSelect,
+      "Correct Selection": correct,
+      "Actual Selection": selected,
+      "Right?": win,
       "Time Elapsed": elapsedT,
-      "Win?": win,
-      "Win Total": wins,
-      "Loss Total": losses,
-      "Earnings": money,
-      "Left Sequential": leftSequentialSelect,
-      "Right Sequential": rightSequentialSelect,
-      "Total Trials": trialTotal
+      "Correct Total": wins,
+      "Wrong Total": losses,
+      "Total Trials": trialNumber
         });
         //use the window alert below if you want to test what is being written to any given value
     //window.alert("Trial #" + trial + "  Amount: " + money);
   };
+
+function goToResults() {
+  elapsedT = millis() - trialOn;
+  var color;
+  if (blue == true) {color = "blue";}
+  if (yellow == true) {color = "yellow";}
+
+  var circlePos;
+  var squarePos;
+  if (ePosition > 500) {circlePos = "right"; squarePos = "left";}
+  if (ePosition < 500) {circlePos = "left"; squarePos = "right";}
+
+  winLoss();
+  trialNumber+=1;
+  //RECORD DATA HERE
+  record_responses(circlePos, squarePos, color, numberSelect, correct, selected, win, elapsedT, wins, losses, trialNumber);
+  results = true;
+
+
+}
+
+
+function winLoss(){
+  if (rightAnswer){
+    wins+=1;
+    win = true;
+  }
+  if(!rightAnswer){
+    losses+=1;
+    win = false;
+  }
+}
+
+function drawShapes() {
+  fill(255, 255, 255);
+  rect(rPosition-howBig/2, height*0.5-howBig/2, howBig, howBig);
+  ellipse(ePosition, height*0.5, howBig, howBig);
+}
+
+function resultText() {
+  fill(0, 0, 0);
+  textSize(60);
+  text(answer, width*0.5-120, 64); 
+  textSize(24);
+  text("To continue press the spacebar...", width*0.5-120, 96);
+}
+
+function sideSelect() {
+  if (random(1)<0.5) {
+    rPosition = left;
+    ePosition = right;
+  } else {
+    rPosition = right;
+    ePosition = left;
+  }
+}
+
+function directionsOn() {
+  fill(0, 0, 0);
+  textSize(24);
+  text("If the number is BLUE and EVEN press 'H'", 200, 420);
+  text("If the number is BLUE and ODD press 'G'", 200, 450);
+  text("If the number is YELLOW and GREATER THAN 5 press 'N'", 200, 480);
+  text("If the number is YELLOW and LESS THAN 6 press 'B'", 200, 510);
+}
+
+function numberOn() {
+  textSize(180);
+  //randomize the side
+  if (newTrial) {
+    if (random(1)<0.5) {
+      textPosition = left;
+    } else {
+      textPosition = right;
+    }
+    var choose = random(1);
+    //randomize the color
+    if (choose<0.5) {
+      r = 0;
+      g = 0;
+      b = 255;
+      blue = true;
+      yellow = false;
+    } if(choose>=0.5) {
+      r = 255;
+      g = 255;
+      b = 0;
+      yellow = true;
+      blue = false;
+    }
+    //random number
+    numberSelect = int(random(10))+1;
+    if (numberSelect%2 == 0) {
+      even = true;
+    } else {
+      even = false;
+    }
+    if (numberSelect >5) {
+      high = true;
+    } else {
+      high = false;
+    }
+    if (blue && even) {
+      correct = 1;
+    }
+    if (blue && !even) {
+      correct = 2;
+    }
+    if (yellow && high) {
+      correct = 3;
+    }
+    if (yellow && !high) {
+      correct = 4;
+    }
+    trialOn = millis();
+  }
+  numberWrite();
+}
+
+function numberWrite() {
+  fill(r, g, b);
+  var offset;
+  if (numberSelect>9){
+    offset = 105;
+  }
+  else{
+    offset = 55;
+  }
+  text(numberSelect, textPosition-offset, height/2+70);
+};
+
 
 var finish = function() {
       
@@ -368,237 +358,3 @@ var endExperiment = function() {
 }
 
 
-
-function upperBanner(){
-    fill(0,0,0,155);
-    rect(width/2-240, 12, 480, 35);
-
-    textSize(20);
-    fill(255);
-}
-
-function lowerBanner(){
-    fill(0,0,0,155);
-    rect(width/2-150, 440, 300, 60);
-      
-    textSize(40);
-    fill(200);
-}
-
-function setLetters(){
-  rightIndex = int(random(lettersSmall.length)); 
-  leftIndex = int(random(lettersSmall.length));
-  
-  while (rightIndex == leftIndex){
-    leftIndex = int(random(lettersSmall.length));
-  }
-  
-  rightLetterSmall = lettersSmall[rightIndex]; 
-  rightLetterBig = lettersBig[rightIndex];
-  leftLetterSmall = lettersSmall[leftIndex];
-  leftLetterBig = lettersBig[leftIndex];
-}
-
-Array.prototype.shuffle = function() {
-    var input = this;
-     
-    for (var i = input.length-1; i >=0; i--) {
-     
-        var randomIndex = Math.floor(Math.random()*(i+1)); 
-        var itemAtIndex = input[randomIndex]; 
-         
-        input[randomIndex] = input[i]; 
-        input[i] = itemAtIndex;
-    }
-    return input;
-}
-
-function imageSelect(){
-  if (random(0,1) < 0.5){
-    colorPos = rightPos;
-    BWPos = leftPos;
-    } else {
-      colorPos = leftPos;
-      BWPos = rightPos;
-    }
-   
-   colorImageSelect = colorShuffle[imageCounter%colorShuffle.length];
-   BWImageSelect = BWShuffle[imageCounter%BWShuffle.length];
-   
-   while (colorImageSelect == BWImageSelect){
-     imageCounter+=1;
-     colorImageSelect = colorShuffle[imageCounter%colorShuffle.length];
-     BWImageSelect = BWShuffle[imageCounter%BWShuffle.length];
-   }
-   imageCounter+=1;
-}
-
-function printOutcome(){
-  fill(255,255,255,100);
-  rect(textPos, 190, 250, 100);
-  textSize(90);
-  fill(0);
-       
-  if(textNum ==1){
-    text("WIN", textPos+38, height/2+13);
-    }
-  if(textNum ==2){
-    text("LOSE", textPos+9, height/2+13);
-    }
-}
-
-function printRound(){
-  textSize(24);
-  fill(0);
-  text("ROUND " + round, 30, 455);
-}
-
-function printEarnings(){
-  //KEEP TRACK OF EARNINGS  
-  ////////////////////////
-    
-  //FOR JAVA VERSION
-  //money = (0.05*wins - 0.05*losses);
-  //FOR JAVASCRIPT VERSION  
-  money = (0.05*wins - 0.05*losses).toFixed(2);
-  lowerBanner();
-  textSize(32);
-  text("TOTAL = $" + money, width/2-122, 440+40);
-}
-
-function leftSelect(){
-  if (randomNumber < winPercentageLeft) {
-    //UNCOMMENT timer
-    //elapsedT = new Date().getTime() - imageOn;
-    win = true;
-      wins +=1;
-      textNum = 1;
-    } else {
-      win = false;
-        losses +=1;
-        textNum = 2;
-       }
-     //add datapoints to save here
-     textPos = 138;
-     selected = "left";
-     trial++;
-     nextTrial = true;
-     leftSequentialSelect+=1;
-     rightSequentialSelect=0;
-     trialTotal++;
-     print("  Total Trials: " + trialTotal);
-   }
-
-function rightSelect(){
-  if (randomNumber <winPercentageRight) {
-    //UNCOMMENT timer
-    //elapsedT = new Date().getTime() - imageOn;
-    win = true;
-      wins +=1;
-      textNum = 1;
-    } else {
-      win = false;
-        losses +=1;
-        textNum = 2;
-        }
-       //add datapoints to save here
-     textPos = width/2+138;
-     selected = "right";
-     trial++;
-     nextTrial = true;
-     leftSequentialSelect=0;
-     rightSequentialSelect+=1;
-     trialTotal++;
-     print("  trial: " + trial);
-     print("Total Trials: " + trialTotal);
-
-}
-
-function printRoundTitle(){
-  background(255);
-  fill(0,0,0,30);
-  rect(200, 200, 624, 120);
-  textSize(90);
-  fill(0);
-  text("ROUND " + (round+1), 310, height/2+13);
-  textSize(30);
-  fill(80);
-  text("Click here to continue", 360, height/2+50);
-}
-
-function updateRound(){
-  winPercentageLeft = random(lowerBound,upperBound);
-  winPercentageRight = random(lowerBound,upperBound);
-  trial=0;
-  round+=1;
-  
-  print("  Round #" + round);
-  print("  Left: "+ winPercentageLeft);
-  print("  Right: " + winPercentageRight);
-}
-
-  //ADD INSTRUCTIONS HERE
-  ///////////////////////
-function instructions(){
-  textSize(30);
-  fill(0);
-  text("Welcome to our test experiment...", 50, 50);
-
-  textSize(13);
-  textStyle(NORMAL);
-  textFont("Helvetica");
-  text("NOTE: This is not a real experiment, we are just testing out the process at this point.", 50, 80);
-  text("Everything should work normally, but data will not be used and the trial length is shorter.", 50, 95);
-  text("In the experiment you will select either the left or right box - each has a different image.", 50, 110);
-  text("When you select the box you will either 'win' or 'lose'.", 50, 125);
-  text("This will result in a bonus of either +$0.05 or - $0.05.", 50, 140);
-  text("A tally will be kept over the course of the trial of your total bonus.", 50, 155);
-  text("If upon completion you have a positive value you will be bonused this amount.", 50, 170);
-  text("If you have a negative amount, this will just be calculated as a ZERO bonus.", 50, 185);
-  text("On the next screen you will practice the process.", 50, 205);
-  text("The actual trial will begin when you see the screen 'Round 01'.", 50, 220);
-  
-  fill(0,0,0,100);
-  rect(362,250, 300,50);
-  fill(0);
-  textSize(24);
-  text("Click here to continue.", 383, 284);
-
-}
-
-function page1() {
-    upperBanner();
-    text("Press the 'G' key to select the left image", width/2-190, 38);
-
-    lowerBanner();
-    text("PRACTICE", width/2-90, 440+45);
-}
-function page2() {
-    upperBanner();
-    text("Press the 'H' key to select the right image", width/2-190, 38);
-
-    lowerBanner();
-    text("PRACTICE", width/2-90, 440+45);
-    
-    fill(255,255,255,100);
-    rect(138, 190, 250, 100);
-    textSize(90);
-    fill(0);
-       
-    text("LEFT", 138+28, height/2+13);
-}
-
-function page3 () {
-    upperBanner();
-    text("Press the 'B' key to begin", width/2-115, 38);
-    
-    lowerBanner();
-    text("PRACTICE", width/2-90, 440+45);
-    
-    fill(255,255,255,100);
-    rect(width/2+113, 190, 300, 100);
-    textSize(90);
-    fill(0);
-       
-    text("RIGHT", width/2+113+18, height/2+13);
-}
